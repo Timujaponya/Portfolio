3e47c46c4f13b7974e434720381d993e9867ff60
Timujaponya <arda.timucinacar@gmail.com>
2026-04-01 09:02:10 +0200

    fix: SIGTERM problem, railway system control

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,15 @@ const { connectDb } = require("./config/db.js"); // MongoDB bağlantısı
 const app = require("./app.js");
 
 const {port} = require("./config/env.js");
+const host = process.env.HOST || "0.0.0.0";
 
 // Async startServer ile DB bağlanıp server başlat
 async function startServer() {
     try {
         await connectDb();
-        app.listen(port, ()=>{
-            console.log(`server running on ${port}`);
-        })
+        app.listen(port, host, ()=>{
+            console.log(`server running on ${host}:${port}`);
+        });
     } catch (err) {
         console.error("DB bağlantı hatası:", err);
     }
